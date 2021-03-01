7f7d5450703c706d98f34dbaf6f5a8cc270957cc
bioscream <77776845+bioscream@users.noreply.github.com>
2021-03-01 22:52:44 +0800

    Add files via upload

---
+++ 23日.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\32\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n", sum);
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char bill1[] = "abc";
+//	char bill2 = '1';
+//	printf("%s\n", bill1);
+//	printf("%c\n", bill2);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	char bill1[] = "abc";
+//	char bill2[] = { 'a','b','c' ,0};
+//	printf("%s\n", bill1);
+//	printf("%s\n", bill2);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
